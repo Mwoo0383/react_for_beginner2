ab1235ca1305950727b01c4a04785f458078d902
Minwoo <cap019@naver.com>
2025-05-18 18:04:58 +0900

    Cleanup function 다루기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import {useState, useEffect} from "react";
 
+function Hello() {
+    useEffect(() => {
+        console.log("hi :)");
+        return () => console.log("bi :(");
+    }, []);
+    return <div>Hello</div>;
+}
+
 function App() {
     const [counter, setValue] = useState(0);
     const [keyword, setKeyword] = useState([""]);
     const onClick = () => setValue((prev) => prev+1);
     const onChange = (event) => setKeyword(event.target.value);
+
+    const [showing, setShowing] = useState(false);
+    const onClick2 = () => setShowing(prev => !prev);
     useEffect(() => {
         console.log("I run only once.");
     }, []);
@@ -19,9 +30,11 @@ function App() {
     }, [counter, keyword]);
   return (
     <div>
-      <input value={keyword} onChange={onChange} type="text" placeholder="Search here" />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+        <input value={keyword} onChange={onChange} type="text" placeholder="Search here" />
+        <h1>{counter}</h1>
+        <button onClick={onClick}>click me</button>
+        {showing ? <Hello /> : null}
+        <button onClick={onClick2}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
