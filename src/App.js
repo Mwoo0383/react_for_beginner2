4dfb30cdc46b540fae767a414316a57f4ca74439
Minwoo <cap019@naver.com>
2025-05-18 18:59:59 +0900

    todo-list 목록 추가하기

--- src/App.js
+++ src/App.js
@@ -1,42 +1,36 @@
-import {useState, useEffect} from "react";
-
-function Hello() {
-    useEffect(() => {
-        console.log("hi :)");
-        return () => console.log("bi :(");
-    }, []);
-    return <div>Hello</div>;
-}
+import { useState } from "react";
 
 function App() {
-    const [counter, setValue] = useState(0);
-    const [keyword, setKeyword] = useState([""]);
-    const onClick = () => setValue((prev) => prev+1);
-    const onChange = (event) => setKeyword(event.target.value);
-
-    const [showing, setShowing] = useState(false);
-    const onClick2 = () => setShowing(prev => !prev);
-    useEffect(() => {
-        console.log("I run only once.");
-    }, []);
-    useEffect(() => {
-        console.log("I run when 'keyword' changes.");
-    }, [keyword]);
-    useEffect(() => {
-        console.log("I run when 'counter' changes.");
-    }, [counter]);
-    useEffect(() => {
-        console.log("I run when counter & keyword change.");
-    }, [counter, keyword]);
-  return (
+    const [toDo, setToDo] = useState("");
+    const [toDos, setToDos] = useState([]);
+    const onChange = (event) => setToDo(event.target.value);
+    const onSubmit = (event) => {
+        event.preventDefault();
+        if (toDo === "") {
+            return;
+        }
+        setToDos((currentArray) => [toDo, ...currentArray]);
+        setToDo("");
+    };
+    console.log(toDos);
+    console.log(toDos.map((item, index) => (
+        <li key={index}>{item}</li>
+    )))
+    return (
     <div>
-        <input value={keyword} onChange={onChange} type="text" placeholder="Search here" />
-        <h1>{counter}</h1>
-        <button onClick={onClick}>click me</button>
-        {showing ? <Hello /> : null}
-        <button onClick={onClick2}>{showing ? "Hide" : "Show"}</button>
+        <h1>My To Dos({toDos.length})</h1>
+        <form onSubmit={onSubmit}>
+            <input onChange={onChange} value={toDo} type="text" placeholder="Write your to do..." />
+            <button>Add To Do</button>
+        </form>
+        <hr />
+        <ul>
+            {toDos.map((item, index) => (
+                <li key={index}>{item}</li>
+            ))}
+        </ul>
     </div>
-  );
+    );
 }
 
 export default App;
